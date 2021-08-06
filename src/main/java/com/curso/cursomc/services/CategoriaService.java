15d5ec14a6f0c828497ae3b72cc7e8d699cecfd5
Rafael Araujo <rafa123839@gmail.com>
2021-08-06 09:33:46 -0300

    Ajustes no endpoin /categorias/{id}

--- src/main/java/com/curso/cursomc/services/CategoriaService.java
+++ src/main/java/com/curso/cursomc/services/CategoriaService.java
@@ -16,7 +16,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new com.curso.cursomc.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }	
